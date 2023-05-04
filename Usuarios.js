3ed112342f29c1e39268b13959430972de430162
Cristian <bcristianromero@gmail.com>
2023-05-04 13:28:58 +0200

    Se añade en JS declaraciones

--- Usuarios.js
+++ Usuarios.js
@@ -5,4 +5,43 @@ let usuario = ["Paco123", "Bernabeu93", "VivaElBetis"];
 let contraseña = ["123456789", "1111111", "Abracadabra"]; 
 let foto = ["Peepo.jpg", "Peepohappy.jpg", "PeepoSad.jpg"];
 let Direccion = ["Calle Falsa 123", "Calle de la Piruleta", "Calle Lafora"]; 
-let telefono = ["123456789", "234567890", "345678901"]; 
+let telefono = ["123456789", "234567890", "345678901"];
+
+let estudiante1 = {
+    DNI: "01234567A",
+    Nombre: "Paco",
+    Apellidos: "Peña",
+    Usuario: "Paco123",
+    Contraseña: "123456789",
+    Foto: "Peepo.jpg",
+    Direccion: "Calle Falsa 123",
+    Telefono: "123456789"
+  };
+  
+  let estudiante2 = {
+    DNI: "12345678B",
+    Nombre: "Cristian",
+    Apellidos: "Bernabeu",
+    Usuario: "Bernabeu93",
+    Contraseña: "1111111",
+    Foto: "Peepohappy.jpg",
+    Direccion: "Calle de la Piruleta",
+    Telefono: "234567890"
+  };
+  
+  let estudiante3 = {
+    DNI: "23456789C",
+    Nombre: "Zeus",
+    Apellidos: "Zamora",
+    Usuario: "VivaElBetis",
+    Contraseña: "Abracadabra",
+    Foto: "PeepoSad.jpg",
+    Direccion: "Calle Lafora",
+    Telefono: "345678901"
+  };
+
+console.log(estudiante1.Nombre); 
+console.log(estudiante2.Direccion); 
+console.log(estudiante3.Contraseña); 
+  
+  
