b4c093ae93a198498a123444a776163fd673caa0
Cristian <bcristianromero@gmail.com>
2023-05-04 14:01:01 +0200

    Se borra declaraciones y se crea constructores

--- Usuarios.js
+++ Usuarios.js
@@ -1,41 +1,12 @@
-let DNI = ["01234567A", "12345678B", "23456789C"]; 
-let Nombre = ["Paco", "Cristian", "Zeus"]; 
-let Apellidos = ["Peña", "Bernabeu", "Zamora"];
-let usuario = ["Paco123", "Bernabeu93", "VivaElBetis"]; 
-let contraseña = ["123456789", "1111111", "Abracadabra"]; 
-let foto = ["Peepo.jpg", "Peepohappy.jpg", "PeepoSad.jpg"];
-let Direccion = ["Calle Falsa 123", "Calle de la Piruleta", "Calle Lafora"]; 
-let telefono = ["123456789", "234567890", "345678901"];
-
-let estudiante1 = {
-    DNI: "01234567A",
-    Nombre: "Paco",
-    Apellidos: "Peña",
-    Usuario: "Paco123",
-    Contraseña: "123456789",
-    Foto: "Peepo.jpg",
-    Direccion: "Calle Falsa 123",
-    Telefono: "123456789"
-  };
-  
-  let estudiante2 = {
-    DNI: "12345678B",
-    Nombre: "Cristian",
-    Apellidos: "Bernabeu",
-    Usuario: "Bernabeu93",
-    Contraseña: "1111111",
-    Foto: "Peepohappy.jpg",
-    Direccion: "Calle de la Piruleta",
-    Telefono: "234567890"
-  };
-  
-  let estudiante3 = {
-    DNI: "23456789C",
-    Nombre: "Zeus",
-    Apellidos: "Zamora",
-    Usuario: "VivaElBetis",
-    Contraseña: "Abracadabra",
-    Foto: "PeepoSad.jpg",
-    Direccion: "Calle Lafora",
-    Telefono: "345678901"
-  };
+function Estudiante(DNI, Nombre, Apellidos, usuario, contraseña, foto, Direccion, telefono, tipo_usuario, activo) {
+    this.DNI = DNI;
+    this.Nombre = Nombre;
+    this.Apellidos = Apellidos;
+    this.usuario = usuario;
+    this.contraseña = contraseña;
+    this.foto = foto;
+    this.Direccion = Direccion;
+    this.telefono = telefono;
+    this.tipo_usuario = tipo_usuario;
+    this.activo = activo;
+  }
