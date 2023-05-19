f85589ea0afdaf68f22395bf88564205a19a0925
Cristian <bcristianromero@gmail.com>
2023-05-19 13:01:33 +0200

    servicios alumnos y mensaje

--- instituto/src/app/alumnos/alumnos.component.ts
+++ instituto/src/app/alumnos/alumnos.component.ts
@@ -1,19 +1,23 @@
-import { alumnos } from './../mock-alumnos';
 import { Component, OnInit } from '@angular/core';
+import { AlumnoCrear } from './../alumnos';
+import { alumnos } from './../mock-alumnos';
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
   styleUrls: ['./alumnos.component.css']
 })
-
 export class AlumnosComponent implements OnInit {
-
-  alumno: alumnos ;
+  listaAlumnos: AlumnoCrear[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.listaAlumnos = alumnos;
   }
 
+  alumnoClick(alumno: AlumnoCrear) {
+    console.log('Alumno clickeado:', alumno);
+    // Aquí puedes realizar la lógica adicional que desees
+  }
 }
--- instituto/src/app/mock-alumnos.ts
+++ instituto/src/app/mock-alumnos.ts
@@ -1,17 +1,20 @@
-import { AlumnoCrear } from './alumnos';
-import { AlumnosComponent } from './alumnos/alumnos.component';
-
-
+export interface AlumnoCrear {
+  id: number;
+  dni: string;
+  nombre: string;
+  apellidos: string;
+  usuario: string;
+  password: string;
+}
 
 export const alumnos: AlumnoCrear[] = [
-  { id: 1, dni:'12345678T', nombre: 'Jorge' ,apellidos: 'García González', usuario:'frodo_bolsón', password:'12345'},
-  { id: 2, dni:'98765432A',nombre: 'Marcos' ,apellidos: 'Rodríguez López',usuario:'arwen_undómiel', password:'abcde'},
-  { id: 3, dni:'54321098H',nombre: 'Ana' ,apellidos: 'Pérez Sánchez',usuario:'samwise_gamgee', password:'qwerty'},
-  { id: 4, dni:'24681012D',nombre: 'Ruben' ,apellidos: 'Martínez Gómez',usuario:'legolas_verdehoja', password:'11111'},
-  { id: 5, dni:'13579024E',nombre: 'Carla' ,apellidos: 'Fernández Ruiz',usuario:'gandalf_el_gris', password:'00000'},
-  { id: 6, dni:'87654321Z',nombre: 'Lorena' ,apellidos: 'Romero Álvarez',usuario:'aragorn_ii', password:'password'},
-  { id: 7, dni:'36925814X',nombre: 'Jesus', apellidos: 'Jiménez Moreno  ',usuario:'bilbo_bolsón', password:'123456'},
-  { id: 8, dni:'48260193P',nombre: 'Cristian' ,apellidos: 'Navarro Ortiz',usuario:'galadriel_lórien', password:'iloveyou'},
-  { id: 9, dni:'97531068J',nombre: 'Isabel' ,apellidos: 'Torres Ramírez',usuario:'eowyn_escudo de roble', password:'letmein'},
-  { id: 10, dni:'71450682S',nombre: 'Ramiro', apellidos: 'Castro Medina',usuario:'gimli_hijo de glóin', password:'monkey'}
-];
+  { id: 1, dni: '12345678T', nombre: 'Jorge', apellidos: 'García González', usuario: 'frodo_bolsón', password: '12345' },
+  { id: 2, dni: '98765432A', nombre: 'Marcos', apellidos: 'Rodríguez López', usuario: 'arwen_undómiel', password: 'abcde' },
+  { id: 3, dni: '54321098H', nombre: 'Ana', apellidos: 'Pérez Sánchez', usuario: 'samwise_gamgee', password: 'qwerty' },
+  { id: 4, dni: '24681012D', nombre: 'Ruben', apellidos: 'Martínez Gómez', usuario: 'legolas_verdehoja', password: '11111' },
+  { id: 5, dni: '13579024E', nombre: 'Carla', apellidos: 'Fernández Ruiz', usuario: 'gandalf_el_gris', password: '00000' },
+  { id: 6, dni: '87654321Z', nombre: 'Lorena', apellidos: 'Romero Álvarez', usuario: 'aragorn_ii', password: 'password' },
+  { id: 7, dni: '36925814X', nombre: 'Jesus', apellidos: 'Jiménez Moreno', usuario: 'bilbo_bolsón', password: '123456' },
+  { id: 8, dni: '48260193P', nombre: 'Cristian', apellidos: 'Navarro Ortiz', usuario: 'galadriel_lórien', password: 'iloveyou' },
+  { id: 9, dni: '97531068J', nombre: 'Isabel', apellidos: 'Torres Ramírez', usuario: 'eowyn_escudo_de_roble', password: 'letmein' },
+  { id: 10, dni: '71450682S', nombre: 'Ramiro', apellidos: 'Castro Medina', usuario: 'gimli_hijo_de_glóin', password: 'monkey' }]
